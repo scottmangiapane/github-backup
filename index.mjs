7571a50de8c4cfec9d8b9895c94df6768104a488
Scott Mangiapane <scottmangiapane@gmail.com>
2022-01-20 13:16:44 -0700

    Fixed permissions issues; better logging

--- index.mjs
+++ index.mjs
@@ -4,10 +4,20 @@ import { promises as fs } from 'fs';
 import fetch from 'node-fetch';
 import path from 'path';
 
+const { STORAGE, TOKEN } = process.env;
+
+async function checkPath(directory) {
+    try {
+        await fs.access(directory);
+        return true;
+    } catch(ignored) {}
+    return false;
+}
+
 async function fetchRepos() {
     const res = await fetch('https://api.github.com/user/repos', {
         method: 'get',
-        headers: { 'authorization': `token ${process.env.TOKEN}` }
+        headers: { 'authorization': `token ${TOKEN}` }
     });
     const repos = await res.json();
     return repos.filter(r => !r.fork);
@@ -17,17 +27,20 @@ function log(line) {
     console.log(`${(new Date()).toUTCString()} - ${line}`);
 }
 
-await fs.mkdir(process.env.STORAGE, { recursive: true });
+await fs.mkdir(STORAGE, { recursive: true });
 
 for (const repo of await fetchRepos()) {
-    const directory = path.join(process.env.STORAGE, repo.name);
-    let command = `git --git-dir "${directory}" remote update --prune`;
-    try {
-        await fs.access(directory);
-    } catch(ignored) {
-        command = `git clone --mirror ${repo.clone_url} "${directory}"`;
+    const directory = path.join(STORAGE, repo.name);
+    if (await checkPath(directory)) {
+        log(`Fetching updates for ${repo.name}`);
+        const command = `git --git-dir "${directory}" remote update --prune`;
+        exec(command);
+    } else {
+        log(`Taking new backup for ${repo.name}`);
+        const [, base] = /^https:\/\/(.*)$/.exec(repo['clone_url']);
+        const url = `https://${TOKEN}@${base}`;
+        const command = `git clone --mirror ${url} "${directory}"`;
+        exec(command);
     }
-    log(command);
-    exec(command);
 }
 
